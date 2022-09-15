9ca0a7ca0afffb03e2e02d391ef72462b3604b6c
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-15 12:14:50 +0200

    create MeasurementsController; implement add(), getMeasurements(), getRainyDaysCount() and handleException() methods

---
+++ WeatherSensorRestApplication/src/main/java/com/andriiv/WeatherSensorRestApplication/controllers/MeasurementsController.java
@@ -0,0 +1,85 @@
+package com.andriiv.WeatherSensorRestApplication.controllers;
+
+import com.andriiv.WeatherSensorRestApplication.dto.MeasurementDTO;
+import com.andriiv.WeatherSensorRestApplication.dto.MeasurementsResponse;
+import com.andriiv.WeatherSensorRestApplication.models.Measurement;
+import com.andriiv.WeatherSensorRestApplication.services.MeasurementService;
+import com.andriiv.WeatherSensorRestApplication.util.MeasurementErrorResponse;
+import com.andriiv.WeatherSensorRestApplication.util.MeasurementException;
+import com.andriiv.WeatherSensorRestApplication.util.MeasurementValidator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
+
+import static com.andriiv.WeatherSensorRestApplication.util.ErrorsUtil.returnErrorsToClient;
+
+/**
+ * @author Roman_Andriiv
+ */
+@RestController
+@RequestMapping("/measurements")
+public class MeasurementsController {
+
+    private final MeasurementService measurementService;
+    private final MeasurementValidator measurementValidator;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MeasurementsController(MeasurementService measurementService,
+                                  MeasurementValidator measurementValidator,
+                                  ModelMapper modelMapper) {
+        this.measurementService = measurementService;
+        this.measurementValidator = measurementValidator;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody @Valid MeasurementDTO measurementDTO,
+                                          BindingResult bindingResult) {
+        Measurement measurementToAdd = convertToMeasurement(measurementDTO);
+
+        measurementValidator.validate(measurementToAdd, bindingResult);
+        if (bindingResult.hasErrors())
+            returnErrorsToClient(bindingResult);
+
+        measurementService.addMeasurement(measurementToAdd);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public MeasurementsResponse getMeasurements() {
+
+        // Usually a list of elements is wrapped into a single object for forwarding
+        return new MeasurementsResponse(measurementService.findAll().stream().map(this::convertToMeasurementDTO)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/rainyDaysCount")
+    public Long getRainyDaysCount() {
+        return measurementService.findAll().stream().filter(Measurement::isRaining).count();
+    }
+
+    private Measurement convertToMeasurement(MeasurementDTO measurementDTO) {
+        return modelMapper.map(measurementDTO, Measurement.class);
+    }
+
+    private MeasurementDTO convertToMeasurementDTO(Measurement measurement) {
+        return modelMapper.map(measurement, MeasurementDTO.class);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<MeasurementErrorResponse> handleException(MeasurementException e) {
+        MeasurementErrorResponse response = new MeasurementErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
