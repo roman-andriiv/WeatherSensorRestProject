e67b0ba7ef3bce26b603c82430dcfd2229233bd6
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-15 10:52:41 +0200

    create MeasurementException and ErrorsUtil

---
+++ WeatherSensorRestApplication/src/main/java/com/andriiv/WeatherSensorRestApplication/util/MeasurementException.java
@@ -0,0 +1,10 @@
+package com.andriiv.WeatherSensorRestApplication.util;
+
+/**
+ * @author Roman_Andriiv
+ */
+public class MeasurementException extends RuntimeException {
+    public MeasurementException(String msg) {
+        super(msg);
+    }
+}
