d4a88c45d1d1a85bf42c6b01ea5ccfb09360a03a
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-15 10:56:24 +0200

    create SensorsController and implement registration() method

---
+++ WeatherSensorRestApplication/src/main/java/com/andriiv/WeatherSensorRestApplication/controllers/SensorsController.java
@@ -0,0 +1,57 @@
+package com.andriiv.WeatherSensorRestApplication.controllers;
+
+import com.andriiv.WeatherSensorRestApplication.dto.SensorDTO;
+import com.andriiv.WeatherSensorRestApplication.models.Sensor;
+import com.andriiv.WeatherSensorRestApplication.services.SensorService;
+import com.andriiv.WeatherSensorRestApplication.util.MeasurementException;
+import com.andriiv.WeatherSensorRestApplication.util.SensorValidator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.andriiv.WeatherSensorRestApplication.util.ErrorsUtil.returnErrorsToClient;
+
+/**
+ * @author Roman_Andriiv
+ */
+@RestController
+@RequestMapping("/sensors")
+public class SensorsController {
+
+    private final SensorService sensorService;
+    private final ModelMapper modelMapper;
+    private final SensorValidator sensorValidator;
+
+    @Autowired
+    public SensorsController(SensorService sensorService, ModelMapper modelMapper,
+                             SensorValidator sensorValidator) {
+        this.sensorService = sensorService;
+        this.modelMapper = modelMapper;
+        this.sensorValidator = sensorValidator;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<HttpStatus> registration(@RequestBody @Valid SensorDTO sensorDTO,
+                                                   BindingResult bindingResult) {
+        Sensor sensorToAdd = convertToSensor(sensorDTO);
+
+        sensorValidator.validate(sensorToAdd, bindingResult);
+
+        if (bindingResult.hasErrors())
+            returnErrorsToClient(bindingResult);
+
+        sensorService.register(sensorToAdd);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private Sensor convertToSensor(SensorDTO sensorDTO) {
+        return modelMapper.map(sensorDTO, Sensor.class);
+    }
+
+
+}
