58e6a3e2d694e87ea470fae6db4149e2126ba65f
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-15 09:57:13 +0200

    initial commit

---
+++ WeatherSensorRestApplication/src/test/java/com/andriiv/WeatherSensorRestApplication/WeatherSensorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriiv.WeatherSensorRestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
